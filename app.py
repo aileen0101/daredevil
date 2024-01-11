814d30a37b0c6ad0cd7243f0b33cb001ab9835e5
aeh245 <aeh245@cornell.edu>
2024-01-10 21:23:34 -0500

    App.py user routes and last goal route

--- app.py
+++ app.py
@@ -27,3 +27,47 @@ def failure_response(message, code=404):
 
 
 # your routes here
+###### user routes ######
+@app.route("/")
+@app.route("/api/users/", methods = ["GET"])
+def get_users():
+    """
+    Endpoint for getting all users
+    """
+    users = [user.serialize() for user in User.query.all()]
+    return success_response ({"users": users}) 
+
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    """
+    Endpoint for creating a new user.
+    """
+    body = json.loads(request.data)
+    input_name = body.get("name")
+    
+    if (
+        input_name is None
+    ):
+        return failure_response("Missing name field!", 400)
+    new_user = User(
+        name=input_name,
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+####### goal routes ######
+@app.route("/api/<int:user_id>/goals/uncompleted/", methods = ["GET"])
+def get_uncompleted_goals():
+    """
+    Endpoint for getting uncompleted goals of a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found")
+    uncompleted_goals = []
+    user_goals = user.serialize().get("goals")
+    for goal in user_goals:
+        if goal.get("done") == False:
+            uncompleted_goals.append(goal)
+    return success_response ({"uncompleted goals": uncompleted_goals}) 
