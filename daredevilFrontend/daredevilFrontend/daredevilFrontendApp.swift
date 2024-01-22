03db94c873a52c63853748ec3dc0f9fde8394237
belle hu <bh552@cornell.edu>
2024-01-22 13:21:20 -0500

    Create xcode project

---
+++ daredevilFrontend/daredevilFrontend/daredevilFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  daredevilFrontendApp.swift
+//  daredevilFrontend
+//
+//  Created by Belle Hu on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct daredevilFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
