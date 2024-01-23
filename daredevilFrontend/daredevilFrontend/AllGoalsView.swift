1127837924ab5111deaaabbde6733f567e9430a9
aeh245 <aeh245@cornell.edu>
2024-01-22 23:12:07 -0500

    Created AllGoalsView

--- daredevilFrontend/daredevilFrontend/AllGoalsView.swift
+++ daredevilFrontend/daredevilFrontend/AllGoalsView.swift
@@ -9,6 +9,111 @@ import SwiftUI
 
 struct AllGoalsView: View {
     var body: some View {
-        Text("Will have vertically scrollable table here")
+        VStack{
+            VStack{
+                List(goals, id: \.self) {
+                    goal in GoalInfoRow(goal)
+                    
+                    
+                }
+                
+            }
+            
+            HorizontalBar
+        }
+        .padding(.top, 10)
+        .navigationTitle(Text("All Dares"))
+}
+
+private func GoalInfoRow(_ goal: Goal) -> some View {
+    
+    ZStack {
+        let color = determineColor(goal)
+        let imageGoal = determineImage(goal)
+        Rectangle()
+            .foregroundStyle(color)
+        HStack{
+            Image(imageGoal)
+                .resizable()
+                .frame(width: 55, height: 55)
+                .padding(.trailing, 100)
+                .offset(x: 12)
+            VStack(alignment: .leading){
+                Text(goal.title)
+                    .fontWeight(.bold)
+                    .font(.system(size: 21))
+                Text(goal.description)
+                    .font(.system(size: 21))
+            }
+            .font(.title)
+            .fontWeight(.light)
+            
+            .offset(x: -10)
+            Spacer()
+        }
     }
+}
+
+    private func determineColor(_ goal: Goal) -> Color {
+        var color : Color
+        if goal.done == true{
+            color = Color.figmaGreen
+        }else{
+            color = Color.figmaBeige
+        }
+        return color
+    }
+    private func determineImage(_ goal: Goal) -> String {
+        var image: String
+        if goal.done == true{
+            image = "done"
+        }else{
+            image = "notDone"
+        }
+        return image
+    }
+    
+private var HorizontalBar: some View {
+    handleIcons
+  }
+
+private var handleIcons: some View {
+    NavigationStack {
+        
+    HStack(spacing: 70) {
+    
+    NavigationLink {
+        ContentView()
+    } label: {
+        backIcon
+    }
+    NavigationLink {
+        NewGoalView()
+    } label: {
+    addIcon
+    }
+    
+    }
+    .foregroundColor(.figmaGreen)
+    .padding(.top, 20)
+    
+
+}
+
+}
+private var backIcon: some View {
+       Image("backButton")
+       .resizable()
+       .frame(width: 57, height: 57)
+ }
+
+private var addIcon: some View {
+       Image("addNew")
+        .resizable()
+        .frame(width: 57, height: 57)
+}
+
+}
+#Preview {
+    AllGoalsView()
 }
--- daredevilFrontend/daredevilFrontend/goal.swift
+++ daredevilFrontend/daredevilFrontend/goal.swift
@@ -7,21 +7,22 @@
 
 import Foundation
 
-struct Goal{
+struct Goal: Hashable {
     let title: String
     let description: String
     let done: Bool
     let userId: Int
+    let image: String
 }
 
 var goals = [
-    Goal(title:"Health", description: "Sleep 8 hours", done: false, userId: 0),
-    Goal(title:"School", description: "Attend lecture", done: false, userId: 0),
-    Goal(title:"Health", description: "Go skiing", done: false, userId: 0),
-    Goal(title:"Nutrition", description: "Try a new fruit", done: false, userId: 0),
-    Goal(title:"Social", description: "Join a dance club", done: false, userId: 0),
-    Goal(title:"Health", description: "Walk 5 miles", done: false, userId: 0),
-    Goal(title:"Fun", description: "Go hiking at Cascadilla Gorge", done: false, userId: 0),
-    Goal(title:"Health", description: "Drink 70 oz water", done: false, userId: 0),
-    Goal(title:"Social", description: "Watch the Boy and the Heron", done: true, userId: 0)
+    Goal(title:"Health", description: "Sleep 8 hours", done: false, userId: 0, image: "pointer"),
+    Goal(title:"School", description: "Attend lecture", done: false, userId: 0,  image: "pointer"),
+    Goal(title:"Health", description: "Go skiing", done: false, userId: 0, image: "pointer"),
+    Goal(title:"Nutrition", description: "Try a new fruit", done: false, userId: 0,  image: "pointer"),
+    Goal(title:"Social", description: "Join a dance club", done: false, userId: 0,  image: "pointer"),
+    Goal(title:"Health", description: "Walk 5 miles", done: false, userId: 0,  image: "pointer"),
+    Goal(title:"Fun", description: "Go hiking at Cascadilla Gorge", done: false, userId: 0,  image: "pointer"),
+    Goal(title:"Health", description: "Drink 70 oz water", done: false, userId: 0, image: "pointer"),
+    Goal(title:"Social", description: "Watch the Boy and the Heron", done: true, userId: 0,  image: "pointer")
 ]
